5c2201e577103386e6625daa8a5fd9659771fff7
ElenaMarushkina <helen.maru@yandex.ru>
2024-01-10 23:32:03 +0300

    My first commit

---
+++ create_user_test.py
@@ -0,0 +1,10 @@
+import sender_stand_request
+    import data
+
+# Тест 11. Ошибка
+# Параметр fisrtName состоит из пустой строки
+def test_create_user_empty_first_name_get_error_response():
+    # В переменную user_body сохраняется обновлённое тело запроса
+    user_body = get_user_body("")
+    # Проверка полученного ответа
+    negative_assert_no_first_name(user_body)
---
+++ pythonProject/configuration.py
@@ -0,0 +1,5 @@
+URL_SERVICE =  https://f9554728-615d-4b32-b98e-37f008954fbe.serverhub.praktikum-services.ru
+DOC_PATH = "/docs/"
+LOG_MAIN_PATH = "/api/logs/main/"
+USERS_TABLE_PATH = "/api/db/resources/user_model.csv".
+CREATE_USER_PATH = "/api/v1/users/" 
